b5076194c9d2d4a6daa990c92e8245d316e4b8f5
yeonwooda <mcsun5@naver.com>
2025-01-04 14:28:10 +0900

    .

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import Navigation from "../components/Header";
 
-const Contact: React.FC = () => {
-  return (
-    <div>
-      <Navigation />
-      <h1>Contact Page</h1>
-      <p>This is the contact page.</p>
-    </div>
-  );
+const page = () => {
+  return <div></div>;
 };
 
-export default Contact;
+export default page;
